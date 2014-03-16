63e6cdd33282b1cb6af34507a22fa64e4dd810f9
Saral Jalan <saraljalan@gmail.com>
2014-03-15 20:25:25 -0700

    Fixing load.js

--- models/Project.js
+++ models/Project.js
@@ -1,21 +1,28 @@
 var mongoose = require( 'mongoose' ),
 	 db = mongoose.createConnection('localhost', 'lacc');
 
-var ProjectSchema = new mongoose.Schema({
-	name: String,
-	address: String,
-	narrative: String,
-	category: String,
-	lat: Number,
-	lng: Number,
-	customFields: 
-	[
-		{
-			key: String,
-			value: String
-		}
-	]
-});
+var ProjectSchema = new mongoose.Schema(
+	{
+		name: String,
+		address: String,
+		narrative: String,
+		category: String,
+		lat: Number,
+		lng: Number,
+		customFields: 
+		[
+			{
+				key: String,
+				value: String,
+				_id: false 
+			}
+		]
+	},
+
+	{versionKey: false}
+	
+
+);
 
 mongoose.model('Project', ProjectSchema );
 mongoose.connect('mongodb://localhost:27017/lacc');
