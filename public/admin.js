92db985863cc32c84c8c026b2fe04e03b4fbc057
Chris McLaughlin <chrismclaughlin55@gmail.com>
2014-03-18 16:07:39 -0700

    edit point works with custom fields

--- public/admin.js
+++ public/admin.js
@@ -8,6 +8,9 @@ var Esri_WorldTopoMap = 'http://server.arcgisonline.com/ArcGIS/rest/services/Wor
 */        
 // Need to refactor this code to make more elegant. This looks like garbage.              
 var updateData = function (m) {
+    for(i in m.data.customFields){
+        console.log(i);
+    }
     document.getElementById('p_id').value = m.data._id;
     document.getElementById('project_name').value = m.data.name;
     document.getElementById('project_category').value = m.data.category;
@@ -16,29 +19,37 @@ var updateData = function (m) {
     document.getElementById('project_lat').value = m.getLatLng().lat;
     document.getElementById('project_lng').value = m.getLatLng().lng;
     var matches = document.querySelectorAll('#entry_list .user_label');
+
+
     for (var i = 0; i < matches.length; ++i) {
         matches.item(i).parentNode.parentNode.removeChild(matches.item(i).parentNode);
     }
-    for (var key in m.data.user_values) {
-        if (m.data.user_values.hasOwnProperty(key)) {
-            var entry = document.createElement('li');
-            var label = document.createElement('input');
-            label.setAttribute('type', 'text');
-            label.setAttribute('placeholder', 'Label');
-            label.setAttribute('class', 'user_label');
-            label.setAttribute('name', 'user_label'+len);
-            label.value = key;
-            var input = document.createElement('input');
-            input.setAttribute('type', 'text');
-            input.setAttribute('placeholder', 'Value');
-            input.setAttribute('class', 'user_value');
-            input.setAttribute('name', 'user_value'+len);
-            input.value = m.data.user_values[key];
-            entry.appendChild(label);
-            entry.appendChild(input);
-            document.getElementById('entry_list').appendChild(entry);
-        }
+    for(i in m.data.customFields){
+        document.getElementById("custom_field_injection_div").innerHTML +='<input name="custom_field_key" type="text" value="'+m.data.customFields[i]['key'] + '"class="field_key"><input name="custom_field_value" class="field_value" type="text" value="' + m.data.customFields[i]['value'] + '"></input>';
+        
+
+
     }
+    // for (var key in m.data.user_values) {
+    //     if (m.data.user_values.hasOwnProperty(key)) {
+    //         var entry = document.createElement('li');
+    //         var label = document.createElement('input');
+    //         label.setAttribute('type', 'text');
+    //         label.setAttribute('placeholder', 'Label');
+    //         label.setAttribute('class', 'user_label');
+    //         label.setAttribute('name', 'user_label'+len);
+    //         label.value = key;
+    //         var input = document.createElement('input');
+    //         input.setAttribute('type', 'text');
+    //         input.setAttribute('placeholder', 'Value');
+    //         input.setAttribute('class', 'user_value');
+    //         input.setAttribute('name', 'user_value'+len);
+    //         input.value = m.data.user_values[key];
+    //         entry.appendChild(label);
+    //         entry.appendChild(input);
+    //         document.getElementById('entry_list').appendChild(entry);
+    //     }
+    // }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -90,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function() {
             console.log("add point value = add_point")
         if(document.getElementById("add_point").value == "edit_point")
             console.log("add point value = edit_point")
-            newMarker.addTo(map);
+        newMarker.addTo(map);
     };
 
     document.getElementById("add_entry").onclick = function() {
@@ -120,16 +131,27 @@ document.addEventListener('DOMContentLoaded', function() {
         socket.on('categories', function(categories) {
             projects.forEach(function(project) {
                 var point = L.marker([project.lat, project.lng], {draggable:'true', clickable:'true'}).addTo(map);
-                    point.data = project;
-                 point.on('click', function() {
-                     updateData(point);
-                 });
-                 point.on('dragend', function(){
+                point.data = project;
+
+                point.on('click', function() {
+                   updateData(point);
+               });
+
+                point.on('dragend', function(){
                     updateData(point);
-                 });
-                 point.on('mouseover', function(){
-                    point.bindPopup(point.data._id).openPopup();
-                 });
+                });
+
+                point.on('mouseover', function(){
+                    var popup = L.popup();
+                    content = '';
+                    var fields = point.data.customFields;
+                    for(i in fields){
+                        content += fields[i]['key']+"->"+fields[i]["value"]+"\n";
+                    }
+                    console.log(content);
+                    point.bindPopup(content).openPopup();
+
+                });
                 // categoryList is a map from category_id to an array of points
                 // project.category is an _id
                 if (categoryMap[project.category]) {
@@ -139,13 +161,13 @@ document.addEventListener('DOMContentLoaded', function() {
                     categoryMap[project.category].push(point);
                 }
             });
-            var overLayMap = {};
-            categories.forEach(function(category) {
-                overLayMap[category.name] = L.layerGroup(categoryMap[category._id]);
-            });
-            L.control.layers(null, overLayMap).addTo(map);
-        });
-    });
-    socket.emit('projectsRequest');
-    socket.emit('categoriesRequest');
+var overLayMap = {};
+categories.forEach(function(category) {
+    overLayMap[category.name] = L.layerGroup(categoryMap[category._id]);
+});
+L.control.layers(null, overLayMap).addTo(map);
+});
+});
+socket.emit('projectsRequest');
+socket.emit('categoriesRequest');
 }, false);
